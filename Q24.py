ce8ab2e0511f8cfdc0bbcfb6c175bdaeb04cfabc
Anextt <97849445+Anextt@users.noreply.github.com>
2023-05-18 11:33:00 -0300

    Create Q24.py

---
+++ Q24.py
@@ -0,0 +1,10 @@
+media = 6
+n1 = int(input("Inserir o primeiro número: "))
+n2 = int(input("Inserir o segundo número: "))
+n3 = int(input("Inserir o terceiro número: "))
+n4 = int(input("Inserir o quarto número: "))
+
+calc = n1 + n2 + n3 + n4
+div = calc // media
+
+print("O resultado é: ", div)
