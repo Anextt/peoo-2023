597c7dfe07a1d9bc8df160383d8e11d5800673bf
Anextt <97849445+Anextt@users.noreply.github.com>
2023-05-18 10:47:34 -0300

    Create Q18.py

---
+++ Q18.py
@@ -0,0 +1,12 @@
+n1 = int(input("Insira um número: "))
+n2 = int(input("Insira outro número: "))
+
+soma = n1 + n2
+mult = n1 * n2
+sub = n1 - n2
+div= n1 / n2
+
+print("Soma: ", soma)
+print("Multiplicação: ", mult)
+print("Subtração: ", sub)
+print("Divisão2: ", div)
