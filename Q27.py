1d8552cf155b6f6664e2d94dc16a516015d6e1f9
Anextt <97849445+Anextt@users.noreply.github.com>
2023-05-18 11:36:58 -0300

    Create Q27.py

---
+++ Q27.py
@@ -0,0 +1,10 @@
+n1 = int(input("Inserir o primeiro número: "))
+n2 = int(input("Inserir o segundo número: "))
+n3 = int(input("Inserir o terceiro número: "))
+n4 = int(input("Inserir o quarto número: "))
+
+calc1 = n1 * n3
+calc2 = n2 + n4
+
+print("O produto é:", calc1)
+print("A soma é:", calc2)
