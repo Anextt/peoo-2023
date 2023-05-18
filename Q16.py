6d58d4473cee72085ceffdfb1f1f2c2db98a51f6
Anextt <97849445+Anextt@users.noreply.github.com>
2023-05-18 10:17:15 -0300

    Create Q16.py

---
+++ Q16.py
@@ -0,0 +1,10 @@
+n1 = int(input("Insira um número: "))
+n2 = int(input("Insira outro número: "))
+
+soma = n1 + n2
+mult = n1 * n2
+sub = n1 - n2
+
+print("Soma: ", soma)
+print("Multiplicação: ", mult)
+print("Subtração; ", sub)
