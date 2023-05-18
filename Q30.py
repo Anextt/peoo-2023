1ebe86dcf40c290d27d9c655557ddbfe6ea85a3e
Anextt <97849445+Anextt@users.noreply.github.com>
2023-05-18 11:45:14 -0300

    Create Q30.py

---
+++ Q30.py
@@ -0,0 +1,8 @@
+ano = int(input("Inserir seu ano de nascimento: "))
+atual = int(input("Inserir o ano atual: "))
+
+id = atual - ano
+futuro = id + 50
+
+print(id, "anos")
+print("Terá: ", futuro, "anos")
