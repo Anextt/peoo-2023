0a9fdd9c4335446da3b6f1941cbdb0a9f20e487e
Anextt <97849445+Anextt@users.noreply.github.com>
2023-10-10 10:09:04 -0300

    Update banco.py

--- banco.py
+++ banco.py
@@ -1,14 +1,15 @@
-#exibe uma mensagem para pedir o cartão e 
-#em seguida as opções
-cart = input("Inserir Seu Cartão De Crédito: ()")
-print(cart["Saque"])
-print(cart["Depósito"])
-print(cart["Transferência"])
-print(cart["Bloqueio"])
-print(cart["Saque"])
+#exibe uma lista para que o usuário selecione o que dedseja
+
+print("====================$====================")
+transacao = ["Saque, Deposito, Transferência, Bloqueio"]
+print(transacao)
+
+info = input("insira a trnsação desejada: ")
+
+cart = input("Inserir número do Cartão De Crédito: ")
 
 #verifica o status do cartão
-if cart < 1:
+if cart != 1 or 2 or 3 or 4 or 5 or 6 or 7 or 8 or 9:
     print("Cartão Recusado, Insira-o Novamente")
 else:
-    print ()
+    print ("_")
